2f17a8f0edf6f81a33452f8039e7b1321b5e1417
Mai Hlee <vangmaihlee@ymail.com>
2022-06-05 00:55:45 -0500

    completed part one and two

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,69 +1,90 @@
 console.log('***** Music Collection *****')
 
-//add function
+//start function 1
+console.log('---Function 1---');
 
 let collection = [];
 
 
 function addToCollection(title, artist, yearPublished){
-    collection.push('albumOne','albumTwo','albumThree','albumFour','albumFive','albumSix','albumSeven');
         return {
-            'albumOne': {
-                title: title,
-                artist:  artist,
-                yearPublished: yearPublished
-            },
-            'albumTwo': {
-                title: 'Crazy',
-                artist: 'Rozes',
-                yearPublished: '2022'
-            },
-            'albumThree': {
-                title: 'Crazy',
-                artist: 'Rozes',
-                yearPublished: '2022'
-            },
-            'albumThree': {
-                title: 'Crazy',
-                artist: 'Rozes',
-                yearPublished: '2022'
-            },
-            'albumFour': {
-                title: 'Crazy',
-                artist: 'Rozes',
-                yearPublished: '2022'
-            },
-            'albumFive': {
-                title: 'Crazy',
-                artist: 'Rozes',
-                yearPublished: '2022'
-            },
-            'albumSix': {
-                title: 'Crazy',
-                artist: 'Rozes',
-                yearPublished: '2022'
-        }
-    };
-
-//collection.push(title, artist, yearPublished);
-
-console.log(addToCollection('The best','Miley','2023'));
-console.log(`${collection}`);
-
-//end funtion
-
-//test function
-
-/*
-let music = {
-    collection.push('albumTwo');
-        return {
-        'albumTwo': {
-            title: 'Crazy',
-            artist: 'Rozes',
-            yearPublished: '2020'
+            title: title,
+            artist:  artist,
+            yearPublished: yearPublished
 
         }
+            
+};
+
+let collectOne = addToCollection('The best','Miley','2023');
+console.log('Miley:', addToCollection('The best','Miley','2023'));
+
+collection.push(collectOne);
+//end function 1
+
+
+//start test function
+console.log('---Test Function 1 ---');
+
+
+let collectTwo = addToCollection('YOU','Ali Gatie','2019');
+console.log('Ali 2019:', addToCollection('YOU','Ali Gatie','2019'));
+collection.push(collectTwo);
+
+let collectThree = addToCollection('The Idea of Her','Ali Gatie','2021');
+console.log('Ali 2021:', addToCollection('The Idea of Her','Ali Gatie','2021'));
+collection.push(collectThree);
+
+let collectFour = addToCollection('Stripped','Faouzia','2020');
+console.log('Faouzia:', addToCollection('Stripped','Faouzia','2020'));
+collection.push(collectFour);
+
+let collectFive = addToCollection('Trying Not To Think About it','JoJo','2021');
+console.log('JoJo:', addToCollection('Trying Not To Think About it','JoJo','2021'));
+collection.push(collectFive);
+
+let collectSix = addToCollection('From the Outside','Hey Violet','2017');
+console.log('Hey Violet:', addToCollection('From the Outside','Hey Violet','2017'));
+collection.push(collectSix);
+
+let collectSeven = addToCollection('Crazy','Rozes','2022');
+console.log('Rozes:', addToCollection('Crazy','Rozes','2022'));
+collection.push(collectSeven);
+
+console.log('Music options:', collection);
+//end test function
+
+//start function 2
+console.log('---Function 2---');
+
+function showCollection(array) {
+    console.log('Numbers in array is:', 7);
+    let i = 0;
+    for (let i=0; i < collection.length; i++){
+        console.log(title, 'by', artist, published in 'yearPublished');
+
     }
+
 }
+
+
+console.log(showCollection());
+
+/*for (let crewMember in spaceship.crew) {
+    console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+  };
 */
+
+//end function 2
+
+
+
+
+//start function 3
+console.log('---Function 3---');
+
+function findByArtist(Faouzia) {
+
+}
+
+//end function 3
